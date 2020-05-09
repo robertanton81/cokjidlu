4da26876d3c9af18ba1b6b18e450bb529816ea9d
Robert Anton <robert.anton@infor.com>
2020-05-09 22:16:40 +0200

    edit controller

--- API/Controllers/RecipeController.cs
+++ API/Controllers/RecipeController.cs
@@ -8,6 +8,7 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Application;
+using System.Threading;
 
 namespace API.Controllers
 {
@@ -23,15 +24,21 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Recipe>>> GetList()
+        public async Task<ActionResult<List<Recipe>>> GetList(CancellationToken ct)
         {
-            return await _mediator.Send(new RecipesList.Query());
+            return await _mediator.Send(new RecipesList.Query(), ct);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Recipe>> GetDetail(Guid id)
         {
             return await _mediator.Send(new RecipeDetail.Query { RecipeId = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> CreateRecipe(CreateRecipe.Command cmd)
+        {
+            return await _mediator.Send(cmd);
         }
     }
 }
---
+++ Application/Recipes/CreateRecipe.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess;
+using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Recipes
+{
+    public class CreateRecipe
+    {
+        public class Command: IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public List<RecipeIngredients> RecipeIngredients { get; set; }
+            public string Instructions { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _ctx;
+
+            public Handler(DataContext ctx)
+            {
+                _ctx = ctx;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var recipe = new Recipe
+                {
+                    Id = request.Id,
+                    Instructions = request.Instructions,
+                    Title = request.Title
+                };
+
+                _ctx.Recipes.Add(recipe);
+                var res = await _ctx.SaveChangesAsync() > 0;
+                if (res)
+                {
+                    return Unit.Value;
+                } 
+                else
+                {
+                    throw new Exception("Error saving changs");
+                }
+            }
+        }
+    }
+}
