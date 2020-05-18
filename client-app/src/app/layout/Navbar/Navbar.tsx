260c78e5156bd0162efbeceffc423771defe7bf2
Robert Anton <robert.anton@infor.com>
2020-05-18 22:30:33 +0200

    css init redux add

---
+++ client-app/src/app/layout/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+    return (
+        <div className={styles.container} >
+            <h1>header</h1>
+        </div>
+    );
+}
